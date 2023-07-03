363839fe1e2318580c1ed4ddfbfe36bf9c00b35f
Adrian Ehrsam <adrian.ehrsam@bmsuisse.ch>
2023-07-03 13:48:35 +0200

    support client id

--- lakeapi2sql/bulk_insert.py
+++ lakeapi2sql/bulk_insert.py
@@ -20,11 +20,16 @@ async def insert_http_arrow_stream_to_sql(connection_string: str, table_name: st
             from azure.identity.aio import DefaultAzureCredential
             credential = DefaultAzureCredential()
         elif auth_part[1] in ["ActiveDirectoryMSI", "ActiveDirectoryManagedIdentity"]:
-            from azure.identity.aio import ManagedIdentityCredential    
-            credential = ManagedIdentityCredential()
+            from azure.identity.aio import ManagedIdentityCredential  
+            client_part = next((p for p in parts if p[0].lower() in ["user", "msiclientid"]), None)
+            if client_part:
+                parts.remove(client_part)
+            credential = ManagedIdentityCredential(client_id=client_part[1] if client_part else None)
         elif auth_part[1] == "ActiveDirectoryInteractive":
             from azure.identity import InteractiveBrowserCredential 
             credential = InteractiveBrowserCredential()
+        elif auth_part[1] == "SqlPassword":# that's kind of an no-op
+            connection_string = ";".join((p[0] + "=" + p[1] for p in parts))
         if credential is not None:
             from azure.core.credentials import AccessToken
             res = credential.get_token("https://database.windows.net/.default")
