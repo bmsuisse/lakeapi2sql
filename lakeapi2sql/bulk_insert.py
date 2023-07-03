891a042ff659006b2898bb60d730be493c376370
Adrian Ehrsam <adrian.ehrsam@bmsuisse.ch>
2023-07-03 13:03:53 +0200

    fix typing

--- lakeapi2sql/bulk_insert.py
+++ lakeapi2sql/bulk_insert.py
@@ -8,6 +8,6 @@ class BulkInfoField(TypedDict):
 class BulkInfo(TypedDict):
     fields: list[BulkInfoField]
 
-async def insert_http_arrow_stream_to_sql(connection_string: str, table_name: str, url: str, basic_auth: tuple[str, str],  aad_token: str|None = None) -> Awaitable[BulkInfo]:
+async def insert_http_arrow_stream_to_sql(connection_string: str, table_name: str, url: str, basic_auth: tuple[str, str],  aad_token: str|None = None) -> BulkInfo:
     return await lvd.insert_arrow_stream_to_sql(connection_string, table_name, url, basic_auth[0], basic_auth[1], aad_token)
 
